b7f44bd0a60bf42ecf27b8a78a45d823c2c8ed4b
dheeraj557 <dheerajchandwani2000@gmail.com>
2021-08-02 08:41:29 +0530

    user suspended

--- db/conn.php
+++ db/conn.php
@@ -20,7 +20,7 @@
     }
     require_once'user.php';
     require_once'crud.php';
-    $crud=new crud($pdo);
-    $user=new user($pdo);
-    $user->insertUser("admin","password");
+//    $crud=new crud($pdo);
+//    $user=new user($pdo);
+//    $user->insertUser("admin","password");
 ?>
